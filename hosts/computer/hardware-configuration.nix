1c0a6bbc0a918b2dc9d792e7871fa13fdc196940
wwxiaoqi <tahicoing@gmail.com>
2024-12-17 11:26:55 +0800

    update: test computer hardware-configuration

--- hosts/computer/hardware-configuration.nix
+++ hosts/computer/hardware-configuration.nix
@@ -4,42 +4,40 @@
 { config, lib, pkgs, modulesPath, ... }:
 
 {
-  imports = [
+  imports = [ ];
 
-  ];
-
-  boot.initrd.availableKernelModules =
-    [ "ata_piix" "ohci_pci" "ehci_pci" "ahci" "sd_mod" "sr_mod" ];
+  boot.initrd.availableKernelModules = [ "ata_piix" "ohci_pci" "ehci_pci" "ahci" "sd_mod" "sr_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ ];
   boot.extraModulePackages = [ ];
 
-  fileSystems."/" = {
-    device = "/dev/disk/by-uuid/cfc7ebe8-e6b9-4ad5-aeef-f4c170d87939";
-    fsType = "btrfs";
-    options = [ "subvol=root" "compress=zstd" ];
-  };
+  fileSystems."/" =
+    { device = "/dev/disk/by-uuid/4934ff73-7b82-4019-8a87-ea99568fb912";
+      fsType = "btrfs";
+      options = [ "subvol=root" "compress=zstd" ];
+    };
 
-  fileSystems."/home" = {
-    device = "/dev/disk/by-uuid/cfc7ebe8-e6b9-4ad5-aeef-f4c170d87939";
-    fsType = "btrfs";
-    options = [ "subvol=home" "compress=zstd" ];
-  };
+  fileSystems."/home" =
+    { device = "/dev/disk/by-uuid/4934ff73-7b82-4019-8a87-ea99568fb912";
+      fsType = "btrfs";
+      options = [ "subvol=home" "compress=zstd" ];
+    };
 
-  fileSystems."/nix" = {
-    device = "/dev/disk/by-uuid/cfc7ebe8-e6b9-4ad5-aeef-f4c170d87939";
-    fsType = "btrfs";
-    options = [ "subvol=nix" "compress=zstd" "noatime" ];
-  };
+  fileSystems."/nix" =
+    { device = "/dev/disk/by-uuid/4934ff73-7b82-4019-8a87-ea99568fb912";
+      fsType = "btrfs";
+      options = [ "subvol=nix" "compress=zstd" "noatime" ];
+    };
 
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/D4E3-E20F";
-    fsType = "vfat";
-    options = [ "fmask=0022" "dmask=0022" ];
-  };
+  fileSystems."/boot" =
+    { device = "/dev/disk/by-uuid/A76B-6AC9";
+      fsType = "vfat";
+      options = [ "fmask=0022" "dmask=0022" ];
+    };
 
   swapDevices =
-    [{ device = "/dev/disk/by-uuid/233e7644-8d71-4958-8111-34050e0ff972"; }];
+    [ { device = "/dev/disk/by-uuid/53d82a92-f1b0-4f9f-a2c1-2e3d655afdc9"; }
+    ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
